a6a5635c353b4f70cad5a10c4c22d5e43dd33f8e
vyshnevska <vyshnevska.n@gmail.com>
2015-12-09 14:41:13 +0100

    Simple search with Elasticsearch

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
+require 'elasticsearch/model'
 class Article < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 end
+Article.import # for auto sync model with elastic search
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :articles
+  get 'search', to: 'search#search'
 end
