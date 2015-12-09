d95e133f31b3d8ff71b3e3faf4a541dc0f921a6a
vyshnevska <vyshnevska.n@gmail.com>
2015-12-09 15:11:10 +0100

    Custom search

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,34 @@ require 'elasticsearch/model'
 class Article < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :title, analyzer: 'english'
+      indexes :content, analyzer: 'english'
+    end
+  end
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title^10', 'content']
+          }
+        }
+      }
+    )
+  end
 end
-Article.import # for auto sync model with elastic search
+# Delete the previous articles index in Elasticsearch
+Article.__elasticsearch__.client.indices.delete index: Article.index_name rescue nil
+
+# Create the new index with the new mapping
+Article.__elasticsearch__.client.indices.create \
+  index: Article.index_name,
+  body: { settings: Article.settings.to_hash, mappings: Article.mappings.to_hash }
+
+# Index all article records from the DB to Elasticsearch
+Article.import
