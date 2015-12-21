feae434a2acf4b0c92cd592584e4365bda82527c
vyshnevska <vyshnevska.n@gmail.com>
2015-12-21 22:54:38 +0100

    Some specs

--- spec/shared_examples/models/concerns/searchable.rb
+++ spec/shared_examples/models/concerns/searchable.rb
@@ -1,9 +1,20 @@
 RSpec.shared_examples 'a searchable' do
 
-  let(:article) { build_stubbed described_class }
+  let!(:article) { create described_class }
 
-  specify {
-    #..more specs
-  }
+  def search attributes = {}
+    Article.search(attributes)
+  end
 
+  before do
+    Article.__elasticsearch__.create_index! index: Article.index_name
+    Article.import
+    sleep 1
+  end
+
+  after {  Article.__elasticsearch__.client.indices.delete index: Article.index_name }
+
+  specify 'search by title' do
+    # expect(search(q: 'some').records).to match [article]
+  end
 end
